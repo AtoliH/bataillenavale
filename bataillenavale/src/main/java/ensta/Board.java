7cd2bc1c9f5e7c12dd922f391b2d90564bbee0f8
Fire <atoli.huppe@ensta-paris.fr>
2021-02-16 11:11:47 +0100

    Exercice 1

---
+++ bataillenavale/src/main/java/ensta/Board.java
@@ -0,0 +1,28 @@
+package ensta;
+
+public class Board {
+    private String nom;
+    private Character[][] navires;
+    private boolean[][] frappes;
+
+    public Board (String nomVoulu, int taille) {
+        this.nom = nomVoulu;
+        navires = new Character[taille][taille];
+        frappes = new boolean[taille][taille];
+    }
+
+    public Board (String nomVoulu) {
+        this(nomVoulu, 10);
+    }
+
+    public String print () {
+        String affichage = "Navires :" + " ".repeat(10) + "Frappes:\n";
+        affichage += "  " + "A B C D E F G H I J" + "    " + "A B C D E F G H I J\n";
+        
+        for (int i = 1; i <= 10; i++) {
+            affichage += i+" " + ". ".repeat(10) + " "+ i+" " + ". ".repeat(10) + "\n";
+        }
+
+        return affichage;
+    }
+}
---
+++ bataillenavale/src/test/java/ensta/TestBoard.java
@@ -0,0 +1,17 @@
+package ensta;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class TestBoard 
+{
+    @Test
+    public void main()
+    {
+        assertTrue( true );
+    }
+}
